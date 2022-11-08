03347480b84a38eb78a5a8e825797d8b9b094d9e
trevorwavelet@gmail.com <trevorwavelet@gmail.com>
2022-11-08 17:45:29 -0500

    created file for algorithms lab

--- Java refresher/src/Main.java
+++ Java refresher/src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int age;
+        age = 27;
+        System.out.println(age);
     }
 }
