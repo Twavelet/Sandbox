62fdb395816f400795664ff6dadb899fffd06537
trevorwavelet@gmail.com <trevorwavelet@gmail.com>
2022-11-10 15:25:10 -0500

    almost finished problem one from advanced algos lab - returned the count of each character in a string - now to filter

---
+++ advanced_algos_practice.py
@@ -0,0 +1,20 @@
+#1: Create a function to detect and print duplicate characters from a given string
+# BONUS: Modify the function to also include the number of times theduplicate character appears in the string
+
+def duplicate_characters(string_to_check):
+    # list = [string_to_check[i] for i in range(0,len(string_to_check))]
+    # list.sort()
+    # sorted_string = ""
+    dict = {}
+    # for element in list:
+    #     sorted_string+=element
+    # print(sorted_string) 
+    for char in string_to_check:
+        dict.update({char: string_to_check.count(char)})
+    print(dict)
+    
+    
+
+
+
+duplicate_characters("oqiwhfujisbdckjvbzxiousdibsguirbgaaaaaaaaaa")
