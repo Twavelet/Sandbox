9476df5bd63879258b3caed740f3d2ac6ec86ec2
trevorwavelet@gmail.com <trevorwavelet@gmail.com>
2022-11-17 14:01:50 -0500

    saving progress

--- Java refresher/src/Main.java
+++ Java refresher/src/Main.java
@@ -1,9 +1,10 @@
+import java.util.Scanner;
 import java.util.stream.IntStream;
 
 public class Main {
     public static void main(String[] args) {
 //       Loops Refresher Lab:
-        for (int a =0; a < 5; a++){
+        for (int a = 0; a < 5; a++){
             System.out.println("Hello");
         }
 
@@ -14,5 +15,16 @@ public class Main {
         for (int c = 10; c>=0; c--){
             System.out.println(c);
         }
+
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("How many times do you want to iterate?");
+        int iterations = userInput.nextInt();
+        System.out.println(iterations);
+        for (int d = 0; d < iterations; d++){
+            System.out.println("Interested logic flow here");
+        }
+
+        
+
     }
 }
