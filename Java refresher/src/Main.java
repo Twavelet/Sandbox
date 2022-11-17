b077ab634dd941f70f5bc9bb554a1af3c303b56f
trevorwavelet@gmail.com <trevorwavelet@gmail.com>
2022-11-17 13:17:24 -0500

    working on Java fundamentals refresher - currently loops

--- Java refresher/src/Main.java
+++ Java refresher/src/Main.java
@@ -1,7 +1,18 @@
+import java.util.stream.IntStream;
+
 public class Main {
     public static void main(String[] args) {
-        int age;
-        age = 27;
-        System.out.println(age);
+//       Loops Refresher Lab:
+        for (int a =0; a < 5; a++){
+            System.out.println("Hello");
+        }
+
+        for(int b = 0; b < 11; b++){
+            System.out.println(b);
+        }
+
+        for (int c = 10; c>=0; c--){
+            System.out.println(c);
+        }
     }
 }
