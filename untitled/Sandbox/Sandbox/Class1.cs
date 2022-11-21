9fdeadd2dae6177d31654e362994fbd6bad3a681
trevorwavelet@gmail.com <trevorwavelet@gmail.com>
2022-11-21 16:36:34 -0500

    messing around with c#

---
+++ untitled/Sandbox/Sandbox/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sandbox
+{
+    public class Can
+    {
+        public string? color;
+        public bool isOpen;
+        public int percentageFull;
+    }
+
+
+}
+
---
+++ untitled/Sandbox/Sandbox/Program.cs
@@ -0,0 +1,11 @@
+﻿
+
+
+int hoursWorked = 35;
+double payRate = 14.25;
+double totalPay = hoursWorked * payRate;
+Console.WriteLine(totalPay);
+
+
+
+
