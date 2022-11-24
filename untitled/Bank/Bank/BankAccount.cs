f35389c1fec216e8fa357bd9efa8219ab6996e96
trevorwavelet@gmail.com <trevorwavelet@gmail.com>
2022-11-23 19:04:31 -0500

    saving progress on C# OOP practice

--- untitled/Bank/Bank/BankAccount.cs
+++ untitled/Bank/Bank/BankAccount.cs
@@ -11,23 +11,58 @@ namespace Bank
         //properties/characteristics (what a bank account has) and what they can do -- "get" == get them & "set" means you cna set them later on
         public string Number { get; }
         public string Owner { get; set; }
-        public decimal Balance { get; }
+        public decimal Balance { 
+            get {
+                decimal balance = 0;
+                foreach (var item in transactionList)
+                {
+                    balance += item.Amount;
+                }
+                return balance;
+            }
+        }
+
+        private int accountNumberSeed = 123456;
+
+        private List<Transaction> transactionList = new List<Transaction>();
 
         public BankAccount(string name, decimal initialBalance)
         {
             this.Owner = name;
-            this.Balance = initialBalance;
+            MakeDeposit(initialBalance, DateTime.Now, "Initial deposit");
+            this.Number = accountNumberSeed.ToString();
+            accountNumberSeed++;
+
         }
 
         //Methods -- what the Bank Account can do
         public void MakeDeposit(decimal amount, DateTime date, string note)
         {
-
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Insufficient amount. The deposit has to be positive.");
+            }
+            var deposit = new Transaction(amount , date, note);
+            transactionList.Add(deposit);
         }
 
         public void MakeWithdrawal(decimal amount, DateTime date, string note)
         {
 
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Withdrawal amount must be positive.");
+
+            }
+
+            if (Balance - amount < 0)
+            {
+                throw new InvalidOperationException("Not enough funds to make this withdrawal");
+            }
+            var withdrawal = new Transaction(-amount , date, note);
+            transactionList.Add(withdrawal);
+            
         }
+
     }
 }
--- untitled/Bank/Bank/Program.cs
+++ untitled/Bank/Bank/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
-
-var trevorsAccount = new Bank.BankAccount("Trevor", 1000);
-Console.WriteLine($"A new account has been opened for: {trevorsAccount.Owner} with an initial balance of {trevorsAccount.Balance}.");
+﻿var trevorsAccount = new Bank.BankAccount("Trevor", 1000);
+Console.WriteLine($"A new account ({trevorsAccount.Number}) has been opened for: {trevorsAccount.Owner} with an initial balance of {trevorsAccount.Balance}.");
+trevorsAccount.MakeWithdrawal(100, DateTime.Now, "For Hammock");
+Console.WriteLine(trevorsAccount.Balance);
